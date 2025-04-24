0e542394986c2b14fb2520a7a8cb072f3289ef2f
MaanavKrishna <67054795+MaanavKrishna@users.noreply.github.com>
2025-04-25 02:19:03 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export const metadata: Metadata = {
     google: "google-site-verification-code",
     yandex: "yandex-verification-code",
     yahoo: "yahoo-verification-code",
-    bing: "msvalidate.01=bing-verification-code",
+    msValidate: "msvalidate.01=bing-verification-code",
     other: {
       "facebook-domain-verification": "facebook-verification-code",
     },
